608e22c1b592ecf5129c13dbebe5af1fec113778
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2024-03-01 19:22:33 -0800

    deployed app and provisioned database

--- src/app/dashboard/logworkout/page.tsx
+++ src/app/dashboard/logworkout/page.tsx
@@ -1,3 +1,25 @@
+import { sql } from "@vercel/postgres";
+
+async function logWorkout({
+	params,
+}: {
+	params: { user: string };
+}): Promise<JSX.Element> {
+	const { rows } =
+		await sql`SELECT * FROM workouts WHERE user = ${params.user}`;
+	return (
+		<div>
+			{rows.map((row) => {
+				return (
+					<div key={row.id}>
+						{row.id} - {row.user}
+					</div>
+				);
+			})}
+		</div>
+	);
+}
+
 export default function Page() {
 	return <h1>This is the log workout page</h1>;
 }
